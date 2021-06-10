4676333b3ad3998e7008a503dbdd528a9f2274be
Akansha Gupta <59836921+agup0102@users.noreply.github.com>
2021-06-10 19:19:38 +1000

    added particle js

--- online-resume/src/App.tsx
+++ online-resume/src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 import FrontUi from "./components/header/front-ui";
 import Header from "./components/header";
 import Pdf from "./assests/resume.pdf";
-
+import Particles from "react-particles-js";
 import ContactUs from "./components/contactUs";
 
 const navigation = {
@@ -19,6 +19,65 @@ const navigation = {
     { name: "Contact", to: "/contact" },
   ],
 };
+const particleConfig: any = {
+  particles: {
+    color: {
+      value: "#014b3e",
+    },
+    number: {
+      value: 160,
+      density: {
+        enable: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        speed: 4,
+        size_min: 0.3,
+      },
+    },
+    line_linked: {
+      color: {
+        value: "#032825",
+      },
+      enable: true,
+      distance: 300,
+      opacity: 0.4,
+      width: 2,
+    },
+    move: {
+      random: true,
+      direction: "top",
+      out_mode: "out",
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "attract",
+      },
+      onclick: {
+        enable: true,
+        mode: "repulse",
+      },
+    },
+    modes: {
+      bubble: {
+        distance: 250,
+        duration: 2,
+        size: 0,
+        opacity: 0,
+      },
+      repulse: {
+        distance: 400,
+        duration: 4,
+      },
+    },
+  },
+};
 const resumeDate = {
   name: "Akansha",
   role: "Software Engineer",
@@ -28,6 +87,7 @@ function App() {
   const { brand, links } = navigation;
   return (
     <>
+      <Particles params={particleConfig} className="particles" />
       <div className="app_background">
         <Header brand={brand} links={links} />
         <FrontUi resumeDate={resumeDate} />
